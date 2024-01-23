742004f158cf3a97422e90575f5054a2461bc714
robin-samuel <robin.s.jung@outlook.com>
2024-01-23 04:25:48 +0100

    feat: added android 5 tls profiles

--- okhttp4.go
+++ okhttp4.go
@@ -6,6 +6,101 @@ import (
 	tls "github.com/bogdanfinn/utls"
 )
 
+var Okhttp4Android5 = profiles.NewClientProfile(
+	tls.ClientHelloID{
+		Client:  "Okhttp4 Android",
+		Version: "5.0.2",
+		Seed:    nil,
+		SpecFactory: func() (tls.ClientHelloSpec, error) {
+			return tls.ClientHelloSpec{
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+					tls.FAKE_TLS_EMPTY_RENEGOTIATION_INFO_SCSV,
+				},
+				CompressionMethods: []uint8{
+					tls.CompressionNone,
+				},
+				Extensions: []tls.TLSExtension{
+					&tls.SNIExtension{},
+					&tls.SupportedPointsExtension{SupportedPoints: []byte{
+						0x00, 0x01, 0x02,
+					}},
+					&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
+						tls.CurveID(14),
+						tls.CurveID(13),
+						tls.CurveP521,
+						tls.CurveID(11),
+						tls.CurveID(12),
+						tls.CurveP384,
+						tls.CurveID(9),
+						tls.CurveID(10),
+						tls.CurveID(22),
+						tls.CurveP256,
+						tls.CurveID(8),
+						tls.CurveID(6),
+						tls.CurveID(7),
+						tls.CurveID(20),
+						tls.CurveID(21),
+						tls.CurveID(4),
+						tls.CurveID(5),
+						tls.CurveID(18),
+						tls.CurveID(19),
+						tls.CurveID(1),
+						tls.CurveID(2),
+						tls.CurveID(3),
+						tls.CurveID(15),
+						tls.CurveID(16),
+						tls.CurveID(17),
+					}},
+					&tls.SessionTicketExtension{},
+					&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
+						tls.PKCS1WithSHA512,
+						0x602,
+						tls.ECDSAWithP521AndSHA512,
+						tls.PKCS1WithSHA384,
+						0x502,
+						tls.ECDSAWithP384AndSHA384,
+						tls.PKCS1WithSHA256,
+						0x402,
+						tls.ECDSAWithP256AndSHA256,
+						0x301,
+						0x302,
+						0x303,
+						tls.PKCS1WithSHA1,
+						0x202,
+						tls.ECDSAWithSHA1,
+					}},
+					&tls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
+				},
+			}, nil
+		},
+	},
+	map[http2.SettingID]uint32{
+		http2.SettingInitialWindowSize: 16777216,
+	},
+	[]http2.SettingID{
+		http2.SettingInitialWindowSize,
+	},
+	[]string{
+		":method",
+		":path",
+		":authority",
+		":scheme",
+	},
+	16711681,
+	nil,
+	nil,
+)
+
 var Okhttp4Android7 = profiles.NewClientProfile(
 	tls.ClientHelloID{
 		Client:  "Okhttp4 Android",
